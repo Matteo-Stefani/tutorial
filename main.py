e9b1390ff157d231a5de2a0c3853afabe324aaa6
matteo <stefani.stmt@gmail.com>
2021-09-03 09:19:08 +0200

    new function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def print_hi(name):
 
     print(f'Hi, {name}')
-
+a = 2
+b = 2
+def myfun(x,y):
+    return x+y
 if __name__ == '__main__':
     print_hi('PyCharm1')
     print('ciao')
+    print(myfun(a,b))
 
